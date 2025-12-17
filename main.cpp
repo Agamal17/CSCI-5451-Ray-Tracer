5f39d645a9b4c1e421ce25432ccff2abf61831aa
Chandan Shankar Mohan <cshankarmwork@gmail.com>
2025-12-16 19:34:41 -0600

    OpenMP code
    Added OpenMP support for parallel processing in ray tracing.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include "Include/scene.h"
 #include <iostream>
 #include <string>
+#include <omp.h>
 #include <chrono>
 #include <iomanip>
 
@@ -36,6 +37,9 @@ int main(int argc, char** argv) {
 
     auto t_total_start = std::chrono::steady_clock::now();
 
+    // OpenMP Implementation
+    omp_set_dynamic(0);
+    #pragma omp parallel for collapse(2)
     for (int i = 0; i < img_width; i++) {
         for (int j = 0; j < img_height; j++) {
             double u = halfW - i + 0.5;
@@ -72,5 +76,6 @@ int main(int argc, char** argv) {
 
     return 0;
 }
+
 
 
